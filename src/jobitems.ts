394534e5625171bb4ce9a0ec722b7e97ec85ae40
Supakit Theanthunyakit(Pok) <s.theanthunyakit@accenture.com>
2024-02-28 23:18:27 +0700

    Update: update jobitem mock

--- src/jobitems.ts
+++ src/jobitems.ts
@@ -50,6 +50,46 @@ const JobItems: JobItem[] = [
         jobDetail: "x1 graphic designer\nx1 stylist\nx1 retoucher",
         image: "https://drive.google.com/thumbnail?id=1W8i3oxalz5x2PRawutn6zLLyu0SWJw9q&sz=w1000",
     },
+    {
+        jobName: "full production content create pack",
+        jobDetail: "x1 producer\nx1 photographer\nx1 retoucher and more",
+        image: "https://drive.google.com/thumbnail?id=12HIspBI8EM1Udtu6Ha9z6TY1e87bsc_7&sz=w1000",
+    },
+    {
+        jobName: "Hospital billboard design",
+        jobDetail: "x1 graphic designer\nx1 stylist\nx1 retoucher",
+        image: "https://drive.google.com/thumbnail?id=12HIspBI8EM1Udtu6Ha9z6TY1e87bsc_7&sz=w1000",
+    },
+    {
+        jobName: "Hotel billboard design",
+        jobDetail: "x1 graphic designer\nx1 stylist\nx1 retoucher",
+        image: "https://drive.google.com/thumbnail?id=1Kfpic8DdKZynK_B9YAcwsv2u6RhtfXQB&sz=w1000",
+    },
+    {
+        jobName: "Magazine design",
+        jobDetail: "x1 graphic designer\nx1 stylist\nx1 retoucher",
+        image: "https://drive.google.com/thumbnail?id=1ts0syrYvo5f6PU5AMq_15LLXDBqbgY7N&sz=w1000",
+    },
+    {
+        jobName: "social media illustration",
+        jobDetail: "x1 graphic designer",
+        image: "https://drive.google.com/thumbnail?id=1ts0syrYvo5f6PU5AMq_15LLXDBqbgY7N&sz=w1000",
+    },
+    {
+        jobName: "social media illustration",
+        jobDetail: "x1 graphic designer",
+        image: "https://drive.google.com/thumbnail?id=1ts0syrYvo5f6PU5AMq_15LLXDBqbgY7N&sz=w1000",
+    },
+    {
+        jobName: "social media illustration",
+        jobDetail: "x1 graphic designer",
+        image: "https://drive.google.com/thumbnail?id=12HIspBI8EM1Udtu6Ha9z6TY1e87bsc_7&sz=w1000",
+    },
+    {
+        jobName: "social media illustration",
+        jobDetail: "x1 graphic designer",
+        image: "https://drive.google.com/thumbnail?id=12HIspBI8EM1Udtu6Ha9z6TY1e87bsc_7&sz=w1000",
+    },
 ]
 
 export default JobItems;
